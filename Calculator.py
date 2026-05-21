d1f66b70dcdcee79660d3466469f2facd931a248
Arpan-me-ios <arpankumar1009@gmail.com>
2026-05-21 22:18:39 +0530

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+print("CALCULATOR")
+print("1. ADD" \
+"2. SUB" \
+"3. DIVIDE" \
+"4. MULTIPLY")
+c = int(input("Enter Choice :"))
+a=int(input("NUMBER 1 : "))
+b=int(input("NUMBER 2 : "))
+if c==1:
+ print(a+b)
