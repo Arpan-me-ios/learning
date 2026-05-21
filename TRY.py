88b6993b1667d847b4f4da9a9ed8e1c0dcce0631
Arpan-me-ios <arpankumar1009@gmail.com>
2026-05-22 00:19:43 +0530

    REMOVED: TRY.py

--- TRY.py
+++
@@ -1,7 +0,0 @@
-from google.cloud import aiplatform
-
-aiplatform.init(project="gen-lang-client-0134822810", location="asia-southeast1")
-
-models = aiplatform.Model.list()
-for model in models:
-    print(model.resource_name, model.display_name)
