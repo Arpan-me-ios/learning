bc77aa21971652e7d8936f01557e724c9a474e3e
Arpan-me-ios <arpankumar1009@gmail.com>
2026-05-22 10:43:49 +0530

    All Moves added

--- Poke_Move.py
+++ Poke_Move.py
@@ -3,5 +3,9 @@ name=input("Enter the name of the Pokemon: ")
 url="https://pokeapi.co/api/v2/pokemon/{name}".format(name=name)
 response = requests.get(url)
 dict1=response.json()
+print("What do you want to know about",name,"?"
+      )
+
 print("One Move of",name, "is:", end=" ")
-print(dict1["moves"][0]["move"]["name"])
+for i in range(len(dict1["moves"])):
+    print(dict1["moves"][i]["move"]["name"])
