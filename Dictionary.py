c8d76c491c9d0d6b71cf38243f53c9edd63833a6
Arpan-me-ios <arpankumar1009@gmail.com>
2026-05-22 08:48:09 +0530

    Dictionary v1.2

--- Dictionary.py
+++ Dictionary.py
@@ -2,8 +2,8 @@ name=['Arpan','Satyarth','Shivam','Rohit']
 age=[19,20,21,22]
 city=['Delhi','Mumbai','Bangalore','Chennai']
 d1={'Name':name,'Age':age,'City':city}
-choice=input("What do you want to know about the students? (Name/Age/City): ")
-if choice in d1:
-    print(d1[choice])
-else:
-    print("Invalid choice.")
+choice=input("Who do you want to know about? ")
+if choice in name:
+    print(d1['Name'][name.index(choice)],"is",d1['Age'][name.index(choice)],"years old and lives in",d1['City'][name.index(choice)]);
+else:    
+    print("Sorry, we don't have information about", choice)
